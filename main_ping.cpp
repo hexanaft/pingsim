82ab478433986a1298ff73a8428cbd6705c906d4
vsuchkov <vsuchkov@stc-spb.ru>
2020-03-16 19:46:17 +0300

    fix seg fault

--- main_ping.cpp
+++ main_ping.cpp
@@ -35,6 +35,7 @@ int main(int argc, char *argv[])
         switch (opt) {
             case 'h': {
                 display_usage();
+                return 0;
             } break;
 
             case 's': {
